212bdbe0a1bd3368dfa02fe0485f118a27078ac7
user <shashi863788@gmail.com>
2023-10-21 23:12:37 +0530

    basic file transfer

---
+++ file_transfer_1st.js
@@ -0,0 +1,6 @@
+const a = require("./index");
+console.log(a);
+a.average(10,20);
+a.percent(30,20);
+
+console.log("hii shashi hear");
---
+++ file_transfer_1st_To_here.js
@@ -0,0 +1,13 @@
+const a = {
+    average: (a,b) => {
+        console.log((a+b)/2);
+    },
+
+    percent:(a,b)=>{
+        console.log((a/b) * 100);
+    },
+    
+   
+}; 
+ 
+module.exports = a;
